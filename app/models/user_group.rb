e8d15f4b55ab81ebe8458f92272328bbf69c6b7e
shimoch <mikadonoosoba2@gmail.com>
2019-08-18 15:44:01 +0900

    delete some needless files

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -1,6 +1,6 @@
 class UserGroup < ApplicationRecord
   belongs_to  :user
   belongs_to  :group
-  # validates   :user_id, uniqueness: {scope: :group_id}
-  # validates   :group_id, uniqueness: {scope: :user_id}
+  validates   :user_id, uniqueness: {scope: :group_id}
+  validates   :group_id, uniqueness: {scope: :user_id}
 end
