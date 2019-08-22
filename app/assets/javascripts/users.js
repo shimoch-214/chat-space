a39438c5d875d8ef15b9a5a40ec0179bd8b0964a
shimoch <mikadonoosoba2@gmail.com>
2019-08-22 17:45:12 +0900

    add a function to search users

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,3 +1,37 @@
-$(function(){
-  $()
+$(document).on('turbolinks:load', function(){
+  $('#user-search-field').on('keyup', function() {
+    function userList(user) {
+      html = 
+      `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+      </div>
+      `
+      return html;
+    }
+
+    var keyword = $(this).val();
+    if (keyword == "") {
+      $('#user-search-result').empty();
+      return
+    }
+    $.ajax({
+      url: '/users',
+      type: 'GET',
+      dataType: 'json',
+      data: { keyword: keyword }
+    })
+    .done(function(users) {
+      $('#user-search-result').empty();
+      users.forEach(function(user) {
+        var html = userList(user);
+        $('#user-search-result').append(html);
+      })
+      
+    })
+    .fail(function() {
+      alert("エラーが発生しました。");
+    })
+  })
 })
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,7 +6,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # This action provides incremental search for registered users by ajax scheme.
   def index
-    @users = User.where('name LIKE(?)'"%#{params[:keyword]}%")
+    @users = User.where('name LIKE(?)',"%#{params[:keyword]}%")
   end
 
   # GET /resource/sign_up
@@ -56,6 +56,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+  # permit the keyword to search users
+  def configure_rendering_params
+    params.permit(:controller, :action)
   end
 
   def update_resource(resource, params)
